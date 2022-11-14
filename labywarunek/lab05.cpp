11f02691b1cd7f005734e4b2d3ebaccfb5dad3a3
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2022-11-14 12:55:21 +0100

    Create lab05.cpp

---
+++ labywarunek/lab05.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void wypisz(vector<int>tab)
+{
+    for (int i = 0; i < tab.size(); i++)
+    {
+        cout << tab[i] << endl;
+    }
+}
+
+int suma(vector<int>tab)
+{
+    int wynik = 0;
+    for (int i = 0; i < tab.size(); i++)
+    {
+        wynik += tab[i];
+    }
+    return wynik;
+}
+
+bool czyNalezy(vector<int>tab, int el)
+{
+    for (int i = 0; i < tab.size(); i++)
+    {
+        if (tab[i] == el)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int index(vector<int>tab, int el)
+{
+    for (int i = 0; i < tab.size(); i++)
+    {
+        if (tab[i] == el)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int abs(int a)
+{
+    if (a < 0)
+    {
+        return a * -1;
+    }
+}
+
+int sumaodl(vector<int>tab, int nr)
+{
+    int wynik = 0;
+    for (int i = 0; i < tab.size(); i++)
+    {
+        wynik += abs(nr - tab[i]);
+    }
+    return wynik;
+}
+
+int najlepszydom(vector<int>tab)
+{
+    int wynik = sumaodl(tab, tab[0]);
+    for (int i = 0; i < tab.size(); i++)
+    {
+        if (wynik > sumaodl(tab, tab[i]))
+        {
+            wynik = sumaodl(tab, tab[i]);
+        }
+        return i;
+    }
+}
+
+
+
+int main()
+{
+    vector<int>tab = { 8, 3, 1, 4, 5, 9 };
+    cout << "-----------------" << endl;
+    wypisz(tab);
+    cout << "-----------------" << endl;
+    cout << suma(tab) << endl;
+    cout << "-----------------" << endl;
+    cout << czyNalezy(tab, 3) << endl;
+    cout << "-----------------" << endl;
+    cout << index(tab, 3) << endl;
+    cout << "-----------------" << endl;
+    int a = -3;
+    cout << sumaodl(tab, 3) << endl;
+    cout << najlepszydom(tab);
+}
